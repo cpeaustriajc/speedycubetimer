98e5db69432e0c705290e5bb23c07b905c9b2a41
cpeaustriajc <cpe.austria.johncarlo@gmail.com>
2024-11-30 21:11:25 +0800

    Move to lib, add side content

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, Fragment } from "react";
 import { randomScrambleForEvent } from "cubing/scramble";
 import {
   Button,
@@ -11,11 +11,8 @@ import {
 import clsx from "clsx";
 import "cubing/twisty";
 import { useLoaderData, useRevalidator } from "react-router";
-
-type Time = {
-  time: number;
-  id: number;
-};
+import { formatTime, computeAverageOf5 } from "lib";
+import type { Time } from "lib";
 
 export function meta() {
   return [
@@ -32,8 +29,10 @@ export async function clientLoader() {
 const Home = () => {
   const scramble = useLoaderData<typeof clientLoader>();
   const revalidator = useRevalidator();
+
   const [isRunning, setIsRunning] = useState(false);
   const [keyPressed, setKeyPressed] = useState(false);
+
   const [time, setTime] = useState<number>(0);
   const [times, setTimes] = useState<Time[]>([]);
 
@@ -86,96 +85,61 @@ const Home = () => {
     }
   }, [isRunning]);
 
-  const formatTime = (timeInSeconds: number) => {
-    const hours = Math.floor(timeInSeconds / 3600);
-    const minutes = Math.floor((timeInSeconds % 3600) / 60);
-    const seconds = Math.floor(timeInSeconds % 60);
-    const milliseconds = Math.floor((timeInSeconds % 1) * 100);
-
-    const parts = [];
-
-    if (hours > 0) {
-      parts.push(hours.toString().padStart(2, "0"));
-    }
-
-    if (hours > 0 || minutes > 0) {
-      parts.push(minutes.toString().padStart(2, "0"));
-    }
-
-    parts.push(seconds.toString().padStart(2, "0"));
-    parts.push(milliseconds.toString().padStart(2, "0"));
-
-    if (parts.length > 2) {
-      return `${parts.slice(0, -2).join(":")}:${parts.slice(-2).join(".")}`;
-    } else {
-      return parts.join(".");
-    }
-  };
-
   function reset() {
     setIsRunning(false);
     setTime(0);
   }
 
-  const computeAverageOf5 = () => {
-    if (times.length < 5) {
-      return null;
-    }
-
-    const last5 = times.slice(-5);
-    const sorted = last5.slice().sort((a, b) => a.time - b.time);
-    sorted.pop();
-    sorted.shift();
-
-    return sorted.reduce((a, b) => a + b.time, 0) / 3;
-  };
-
   return (
-    <main
-      className={clsx(
-        isRunning && "bg-green-400",
-        keyPressed && "bg-yellow-400",
-        "h-dvh flex flex-col justify-center items-center"
-      )}
-    >
-      <twisty-player
-        alg={scramble}
-        visualization="2D"
-        background="none"
-        control-panel="none"
-      ></twisty-player>
-      <pre className="text-9xl">{formatTime(time)}</pre>
-      <p>{scramble}</p>
-      <Table className="table-auto" aria-label="Times">
-        <TableHeader>
-          <Column isRowHeader>Time</Column>
-          <Column>Ao5</Column>
-          <Column>Actions</Column>
-        </TableHeader>
-        <TableBody>
-          {times.map((time, index) => (
-            <Row key={index}>
-              <Column>{formatTime(time.time)}</Column>
-              <Column>
-                {times.length >= 5 && index >= times.length - 5
-                  ? formatTime(computeAverageOf5()!)
-                  : "-"}
-              </Column>
-              <Column>
-                <Button
-                  onPress={() => {
-                    setTimes(times.filter((t) => t.id !== time.id));
-                  }}
-                  className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-red-400 transition-colors bg-red-200 text-red-900 data-[hovered]:bg-red-400/90 h-10 px-4 py-2"
-                >
-                  Delete
-                </Button>
-              </Column>
-            </Row>
-          ))}
-        </TableBody>
-      </Table>
-    </main>
+    <div className="h-dvh">
+      <aside>
+        <Table className="table-auto" aria-label="Times">
+          <TableHeader>
+            <Column isRowHeader>Time</Column>
+            <Column>Ao5</Column>
+            <Column>Actions</Column>
+          </TableHeader>
+          <TableBody>
+            {times.map((time, index) => (
+              <Row key={index}>
+                <Column>{formatTime(time.time)}</Column>
+                <Column>
+                  {times.length >= 5 && index >= times.length - 5
+                    ? formatTime(computeAverageOf5(times)!)
+                    : "-"}
+                </Column>
+                <Column>
+                  <Button
+                    onPress={() => {
+                      setTimes(times.filter((t) => t.id !== time.id));
+                    }}
+                    className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-red-400 transition-colors bg-red-200 text-red-900 data-[hovered]:bg-red-400/90 h-10 px-4 py-2"
+                  >
+                    Delete
+                  </Button>
+                </Column>
+              </Row>
+            ))}
+          </TableBody>
+        </Table>
+      </aside>
+      <main
+        className={clsx(
+          isRunning && "bg-green-400",
+          keyPressed && "bg-yellow-400",
+          "flex flex-col justify-center items-center"
+        )}
+      >
+        <twisty-player
+          alg={scramble}
+          visualization="2D"
+          background="none"
+          control-panel="none"
+        ></twisty-player>
+        <pre className="text-9xl">{formatTime(time)}</pre>
+        <p>{scramble}</p>
+      </main>
+    </div>
   );
 };
 
---
+++ lib/index.ts
@@ -0,0 +1,43 @@
+export type Time = {
+  time: number;
+  id: number;
+};
+
+export function formatTime(timeInSeconds: number) {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = Math.floor(timeInSeconds % 60);
+    const milliseconds = Math.floor((timeInSeconds % 1) * 100);
+
+    const parts = [];
+
+    if (hours > 0) {
+        parts.push(hours.toString().padStart(2, "0"));
+    }
+
+    if (hours > 0 || minutes > 0) {
+        parts.push(minutes.toString().padStart(2, "0"));
+    }
+
+    parts.push(seconds.toString().padStart(2, "0"));
+    parts.push(milliseconds.toString().padStart(2, "0"));
+
+    if (parts.length > 2) {
+        return `${parts.slice(0, -2).join(":")}:${parts.slice(-2).join(".")}`;
+    } else {
+        return parts.join(".");
+    }
+}
+
+export function computeAverageOf5(times: Time[]) {
+    if (times.length < 5) {
+        return null;
+    }
+
+    const last5 = times.slice(-5);
+    const sorted = last5.slice().sort((a, b) => a.time - b.time);
+    sorted.pop();
+    sorted.shift();
+
+    return sorted.reduce((a, b) => a + b.time, 0) / 3;
+}
