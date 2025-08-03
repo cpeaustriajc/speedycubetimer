59a065c176a845804a91206e12a59ebf436f9d96
cpeaustriajc <cpe.austria.johncarlo@gmail.com>
2025-08-03 12:35:01 +0800

    feat(alert): add beta warning alert to the application

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,12 @@
 <template>
     <UApp>
+        <UAlert
+            color="warning"
+            variant="outline"
+            title="Product is still in beta"
+            description="Changes are frequent and are still unstable. The final product may differ from the current version."
+            icon="i-lucide-alert-triangle"
+        />
         <NuxtLayout>
             <NuxtPage />
         </NuxtLayout>
