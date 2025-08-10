c73c08e888c68d7c508f3e5f92f1ad8a05169de6
cpeaustriajc <cpe.austria.johncarlo@gmail.com>
2025-08-10 18:26:19 +0800

    chore: remove unused schemas

--- shared/utils/loginSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export default z.object({
-    email: z.email(),
-    password: z
-        .string()
-        .min(6, 'Password must be at least 6 characters long'),
-});
